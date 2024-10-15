5492f4123c3f38c84f60befb10758c5118a1363e
rajni.algorisys <rajni.algorisys@ext.idfy.com>
2024-10-16 00:44:05 +0530

    Added admin routes for adding product, show product details, creating order, edit product

--- src/App.js
+++ src/App.js
@@ -9,6 +9,10 @@ import ProtectedRoute from "./components/ProtectedRoute";
 import AdminRoute from "./components/AdminRoute";
 import PublicRoute from "./components/PublicRoute";
 import Navbar from "./components/navbar/Navbar";
+import AddProduct from "./components/products/AddProduct";
+import ProductDetails from "./components/products/ProductDetails";
+import CreateOrder from "./components/order/CreateOrder";
+import EditProduct from "./components/products/EditProduct";
 
 function App() {
   return (
@@ -16,15 +20,26 @@ function App() {
       <AuthProvider>
         <Navbar />
         <Routes>
+          <Route element={<PublicRoute />}></Route>
+
           <Route path="/signin" element={<Signin />} />
           <Route path="/signup" element={<SignUp />} />
 
+          {/* Protected routes - for logged in users */}
           <Route element={<ProtectedRoute />}>
             <Route path="/products" element={<Products />} />
+            <Route path={`/products/show/:id`} element={<ProductDetails />} />
+            <Route path={`/order/:id/:quantity?`} element={<CreateOrder />} />
+            <Route
+              path={`/products/edit/:productId`}
+              element={<EditProduct />}
+            />
           </Route>
 
-          <Route element={<AdminRoute />}></Route>
-          <Route element={<PublicRoute />}></Route>
+          {/*  Admin routes */}
+          <Route element={<AdminRoute />}>
+            <Route path="/add-product" element={<AddProduct />} />
+          </Route>
         </Routes>
       </AuthProvider>
     </Router>
